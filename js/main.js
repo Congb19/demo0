be25cd3d4ffd443d235d1cdc842823f0facdbc43
Congb19 <347006087@qq.com>
2019-08-09 22:17:52 +0800

    8.9 add pagechange

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#qiehuan2").hide();
+});
+function tomain1() {
+    $("#qiehuan1").click();
+}
+function tomian2() {
+    $("#qiehuan2").click();
+}
+function tomian3() {
+    $("#qiehuan3").click();
+}
